328f42c8c1bb9fcf064765efdd090c49da8e2f60
Art-ylem <art-ylem@mail.ru>
2019-03-11 15:34:55 +0500

    create WhatsApp Layout

---
+++ app/src/main/java/com/example/lesson3/javaExample/WhatsAppActivity.java
@@ -0,0 +1,15 @@
+package com.example.lesson3.javaExample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lesson3.R;
+
+public class WhatsAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whats_upp);
+    }
+}
